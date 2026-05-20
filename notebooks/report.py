bd0f118031abc439456767f51a0c247c799d8f1c
TyAlbao <talbao@ucsd.edu>
2026-05-20 16:32:20 -0700

    added marimo notebook

---
+++ notebooks/report.py
@@ -0,0 +1,30 @@
+import marimo
+
+__generated_with = "0.23.6"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    return pd, plt
+
+
+@app.cell
+def _(pd):
+    df = pd.read_csv('../data/features/events.csv')
+    return (df,)
+
+
+@app.cell
+def _(df, plt):
+    plt.hist(df['duration_minutes'])
+    plt.show()
+    return
+
+
+if __name__ == "__main__":
+    app.run()
